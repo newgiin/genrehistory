fd3888f0ce4b2cb6e6d0658e24df59ab47df7b4e
Andrew Nguyen <atnguyen4@gmail.com>
2015-12-22 20:11:22 -0500

    Add delete user functionality to admin panel.

--- admin.py
+++ admin.py
@@ -1,8 +1,12 @@
 import webapp2
 import os
 import jinja2
+from tag_svc import config
 from tag_svc import models
 from tag_svc.lastfm import LastFm
+
+from google.appengine.ext import ndb
+
 
 JINJA_ENVIRONMENT = jinja2.Environment(
         loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -19,7 +23,7 @@ class MainPage(webapp2.RequestHandler):
             resp = lfm_api.auth_getsession(token)
 
             if 'error' in resp:
-                template_values['auth_error'] = resp['message']
+                template_values['auth_error'] = resp['error']
             else:
                 template_values['auth_user'] = resp['session']['name']
                 template_values['auth_session_key'] = resp['session']['key']
@@ -28,7 +32,8 @@ class MainPage(webapp2.RequestHandler):
                     session_key=resp['session']['key'], namespace='admin')
                 session_entity.put()
         else:
-            lfm_session = models.LastFmSession.get_by_id(LastFm.API_KEY, namespace='admin')
+            lfm_session = models.LastFmSession.get_by_id(LastFm.API_KEY,
+                                                         namespace='admin')
             if lfm_session is not None:
                 template_values['auth_user'] = lfm_session.user
                 template_values['auth_session_key'] = lfm_session.session_key
@@ -38,3 +43,25 @@ class MainPage(webapp2.RequestHandler):
         self.response.write(template.render(template_values))
 
 app = webapp2.WSGIApplication([('/admin', MainPage)], debug=True)
+
+
+class DeleteUserHandler(webapp2.RequestHandler):
+    def post(self):
+        if 'user' in self.request.params:
+            user = self.request.params['user']
+            user_entity = models.User.get_by_id(user,
+                                                namespace=config.DS_VERSION)
+            if user_entity:
+                query = ndb.Query(ancestor=user_entity.key).iter(keys_only=True)
+                ndb.delete_multi(query)
+                self.response.write('Successfully delete user %s' % user)
+            else:
+                self.response.write('User %s does not exist.' % user)
+        else:
+            self.response.write('User param missing.')
+        self.response.write('<br/><a href="%s">Back</a>' % self.request.referer)
+
+
+
+delete_user = webapp2.WSGIApplication([('/delete_user', DeleteUserHandler)],
+                                      debug=True)
